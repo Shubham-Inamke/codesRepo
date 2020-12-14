4a81f6a0a6b5f1c0004002b20388b92f8190713e
Shubham-Inamke <75987781+Shubham-Inamke@users.noreply.github.com>
2020-12-14 18:54:32 +0530

    static variables added

--- add.java
+++ add.java
@@ -1,8 +1,13 @@
 public class Add {
+    static int a1 = 20;
+    static int a2 = 40;
+    
     public static void main(String [] args) {
         int num1 = 10;
         int num2 = 70;
         int sum = num1 + num2;
         System.out.println("Addtion of num1 and num2 is: "+sum);
+        
+        System.out.println("Addition of static variables is: "+(a1+a2));
     }
 }
