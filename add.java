47f7993db73917fa3789eeb7c043d8fd2469a2a9
Shubham-Inamke <75987781+Shubham-Inamke@users.noreply.github.com>
2020-12-15 15:06:55 +0530

    ready to merge

--- add.java
+++ add.java
@@ -11,5 +11,7 @@ public class Add {
         System.out.println("Addition of static variables is: "+(a1+a2));
         System.out.println("Multiplication two numbers is: "+(a1*a2));
         System.out.println("Division of two numbers is: "+(num1/num2));
+        
+        //ready to merge
     }
 }
